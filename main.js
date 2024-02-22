5fa136a3a530396f9ddc3327d78012f89f0861d2
Parkseolmin <qkrtjfals2@naver.com>
2024-02-22 18:37:47 +0900

    함수설정

--- main.js
+++ main.js
@@ -5,6 +5,41 @@ const ODLO_COUNT = 5;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__time');
+const gameScore = document.querySelector('.game__score');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+const showTimerAndScore = () => {
+   gameTimer.style.visibility = 'visible';
+   gameScore.style.visibility = 'visible';
+};
+
+const showStopButton = () => {
+   const icon = gameBtn.querySelector('.fa-play');
+   icon.classList.add('fa-stop');
+   icon.classList.remove('fa-play');
+};
+
+const stopGame = () => {};
+
+const startGame = () => {
+   initGame();
+   showStopButton();
+   showTimerAndScore();
+};
+
+gameBtn.addEventListener('click', () => {
+   if (started) {
+      stopGame();
+   } else {
+      startGame();
+   }
+   started = !started;
+});
 
 const randomNumber = (min, max) => {
    return Math.random() * (max - min) + min;
@@ -30,6 +65,8 @@ const addItem = (className, count, imgPath) => {
 };
 
 const initGame = () => {
+   field.innerHTML = '';
+   gameScore.innerHTML = WALLY_COUNT;
    addItem('wally', WALLY_COUNT, 'img/wally.png');
    addItem('odlo', ODLO_COUNT, 'img/odlo.png');
 };
